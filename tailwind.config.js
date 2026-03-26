4fb96acb543887ccc97cac7bac03f898c6bfc1ac
vargasjayarjarlego-star <vargasjayarjarlego@gmail.com>
2026-03-26 23:26:19 +0800

    Create tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1F2937',
+        secondary: '#3B82F6',
+        accent: '#F59E0B',
+      }
+    },
+  },
+  plugins: [],
+}
